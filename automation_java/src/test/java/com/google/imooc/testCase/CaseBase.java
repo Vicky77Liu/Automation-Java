891720a7f986f6e2c5fde75bb7372e89e8bb93fb
Yiqi Liu <lyq770421@gmail.com>
2022-10-02 21:17:26 -0700

    remove java

--- automation_java/src/test/java/com/google/imooc/testCase/CaseBase.java
+++
@@ -1,9 +0,0 @@
-package com.google.imooc.testCase;
-
-import com.google.imooc.base.DriverBase;
-
-public class CaseBase {
-	public DriverBase initDriver(String browserName) {
-		return new DriverBase(browserName);
-	}
-}
